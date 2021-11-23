06a3fccc0104275891ecabecd2b9c2b64d5b5729
jaschan66 <57925035+jaschan66@users.noreply.github.com>
2021-11-23 16:55:15 +0800

    changes on phpmailer credentials

--- Testing/phpmailer/credential.php
+++ Testing/phpmailer/credential.php
@@ -1,7 +1,6 @@
 <?php
-    // define('EMAIL', 'jaschan65@gmail.com');
-    // define('PASS', 'clkclkclk');
+     define('EMAIL', 'jaschan65@gmail.com');
+     define('PASS', 'clkclkclk');
 
-    define('EMAIL', 'brendanlol123@gmail.com');
-    define('PASS', 'Brendan123706');
+    
 ?>
